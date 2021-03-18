954d78d19dff061d227eb014f33e57798a72ce53
Jenifer Vivar <jenifervivar@icloud.com>
2021-03-18 17:17:49 +0000

    I had to change the + operator to be of type class

--- main.cpp
+++ main.cpp
@@ -5,7 +5,89 @@
 //------------------------------
 
 // Write the assignment code here
+class Real{
+  protected:
+  float real_num;
+  public:
+  Real(float realnum = 1){
+    real_num = realnum;
+  }
+  double GetReal(){
+    return real_num;
+  }
+  
+  Real& operator+(float num){
+    real_num += num;
+    return *this;
+  }
+  //void operator +(float num){
+    //real_num += num;
+    //return real_num;
+  //}
+};
+class Complex : public Real{
+  protected:
+  float imag_num;
+  //double real_num;
 
+  public:
+  Complex(float real = 1, float imagnum = 1){
+    real_num = real;
+    imag_num = imagnum;
+  }
+  double GetImaginary(){
+    return imag_num;
+  }
+
+  Complex& operator+(float num){
+    real_num += num;
+    imag_num += num;
+    return *this;
+  }
+  
+  //friend void operator+( Real b, float num){
+    //b.real_num += num;
+
+    //return b.real_num;
+  //}
+  //void operator+(Complex b){
+    //imag_num += .num;
+    //real_num += num;
+    //return imag_num;
+    //return real_num;
+  //}
+};
+class Surreal : public Complex {
+
+  protected:
+  float surr_num;
+
+  public:
+
+  Surreal(float real, float imag, float surr){
+    real_num = real;
+    imag_num = imag;
+    surr_num = surr;
+  }
+  float GetSurreal(){
+    return surr_num;
+  }
+
+  Surreal& operator+(float num){
+    real_num += num;
+    imag_num += num;
+    surr_num += num;
+
+    return *this;
+  }
+  /*void operator+(float num){
+    real_num += num;
+    imag_num += num;
+    surr_num += num;
+  }*/
+  
+
+};
 
 //------------------------------
 //   DO NOT MODIFY TEST CASES
